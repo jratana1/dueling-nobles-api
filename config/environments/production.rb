6825ae7388ee8ba9cb51dbf975f85d07d2af65a3
John Ratana <jratana1@gmail.com>
2021-08-03 04:05:44 -0400

    cors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://damp-eyrie-01599.herokuapp.com/', 'https://jratana1.github.io/dueling-nobles/#/', 'https://jratana1.github.io', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://damp-eyrie-01599.herokuapp.com/', 'https://jratana1.github.io/dueling-nobles/#/', 'https://jratana1.github.io', /http:\/\/example.*/, * ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
